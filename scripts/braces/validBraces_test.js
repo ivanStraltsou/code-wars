143cbf4dba1fb4339806d27b1aea09378c140967
Ivan Straltsou <ivan_straltsou@epam.com>
2014-09-22 16:41:57 +0300

    travis config added

--- scripts/braces/validBraces_test.js
+++ scripts/braces/validBraces_test.js
@@ -8,12 +8,20 @@ describe('validBraces test', function() {
     expect(validBraces('(){}[]')).toBe(true);
   });
 
+  it('(a)a{}b[b]b: true', function() {
+    expect(validBraces('(a)a{}b[b]b')).toBe(true);
+  });
+
   it('{(): false', function() {
     expect(validBraces('{()')).toBe(false);
   });
 
   it('[(]): false', function() {
     expect(validBraces('[(])')).toBe(false);
+  });
+
+  it('[(a])a: false', function() {
+    expect(validBraces('[(a])a')).toBe(false);
   });
 
   it('([{}]): true', function() {
