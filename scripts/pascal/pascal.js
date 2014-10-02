99e93d7fd75a20b78d8affaa1b8a6d16cc4541bf
Ivan Straltsou <ivan_straltsou@epam.com>
2014-10-02 18:19:59 +0300

    dummy array moved to function

--- scripts/pascal/pascal.js
+++ scripts/pascal/pascal.js
@@ -1,23 +1,25 @@
 function pascal(depth) {
-  var pyramid = [];
+  var pyramid = [],
+      dummyArray = function(length) {
+        return Array.apply(null, Array(length));
+      };
 
   depth = Number(depth);
 
   if (depth) {
-    Array.apply(null, Array(depth)).reduce(function(lastLevel, currentLevel, levelIndex) {
-      var currentLevelLength = levelIndex + 1;
+    dummyArray(depth).reduce(function(lastLevel, currentLevel, levelIndex) {
 
-      currentLevel = Array.apply(null, Array(currentLevelLength))
-          .map(function(value, index) {
+      // create level
+      currentLevel = dummyArray(levelIndex + 1).map(function(value, index) {
 
-            // track boundary indexes
-            return index > 0 && index < levelIndex ?
-                   lastLevel[index - 1] + lastLevel[index] :
-                   1;
-          });
+        // track boundary indexes
+        return (index > 0 && index < levelIndex) ? lastLevel[index - 1] + lastLevel[index] : 1;
+      });
 
+      // save it
       pyramid.push(currentLevel);
 
+      // pass to create the next one
       return currentLevel;
     }, [1]);
   }
