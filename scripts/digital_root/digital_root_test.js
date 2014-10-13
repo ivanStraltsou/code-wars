a859a8382e63ba62a1bfdf02cc28e38aa418517d
Ivan Straltsou <ivan_straltsou@epam.com>
2014-10-13 08:56:28 +0300

    digital root tests updated

--- scripts/digital_root/digital_root_test.js
+++ scripts/digital_root/digital_root_test.js
@@ -2,6 +2,8 @@ describe('digital root tests', function() {
   it('single sum', function() {
     expect(digital_root(431)).toBe(8);
     expect(digital_root(134)).toBe(8);
+    expect(digital_root(0)).toBe(0);
+    expect(digital_root(3)).toBe(3);
     expect(digital_root(341)).toBe(8);
     expect(digital_root(900)).toBe(9);
   });
@@ -11,5 +13,9 @@ describe('digital root tests', function() {
     expect(digital_root(942)).toBe(6);
     expect(digital_root(249)).toBe(6);
     expect(digital_root(429)).toBe(6);
+  });
+
+  it('triple sum', function() {
+    expect(digital_root(9999991)).toBe(1);
   });
 });
