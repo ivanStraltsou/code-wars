0d82e36a2c53df29a08de13ebac39274c8353816
Ivan Straltsou <ivan_straltsou@epam.com>
2014-11-17 01:49:49 +0300

    $ tests update

--- scripts/$/$_test.js
+++ scripts/$/$_test.js
@@ -1,7 +1,16 @@
 describe('prototype', function() {
-  it('init', function() {
+  it('prototype chain', function() {
+    expect('html' in $('a')).toBe(true);
+    expect($('a').hasOwnProperty('html')).toBe(false);
+
+    expect('addClass' in $('a')).toBe(true);
+    expect($('a').hasOwnProperty('addClass')).toBe(false);
+
     expect(typeof $('a').html).toBe('function');
     expect(typeof $('a').addClass).toBe('function');
+  });
+
+  it('addClass, html function tests', function() {
     expect($('a').addClass('classA classB classC').classNames_.length).toBe(3);
     expect($('a').addClass(123).html('test').html()).toBe('test');
   });
