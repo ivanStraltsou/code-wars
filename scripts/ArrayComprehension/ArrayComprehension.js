b2ce0f2ccf802f4d449eabb234938082341074b4
Ivan Straltsou <ivan_straltsou@epam.com>
2014-12-07 16:52:40 +0300

    [GREEN] Array comperehension initial implementation.

--- scripts/ArrayComprehension/ArrayComprehension.js
+++ scripts/ArrayComprehension/ArrayComprehension.js
@@ -1,3 +1,37 @@
-function ArrayComprehension() {
-  return;
+function ArrayComprehension(options) {
+  var generator, start, startIndex, step, parts;
+  var output = [];
+
+  function toNumber(item) {
+    return Number(item);
+  }
+
+  function generate(start, end, step) {
+    step = step || 1;
+
+    return (end < start && step > 0) || (end > start && step < 0)?
+           [] :
+           Array.apply(null, new Array(Math.floor((end - start) / step) + 1)).map(function(value, index) {
+             return start + step * index;
+           });
+  }
+
+  // if generator is specified we expect the format to be correct
+  if (options && options.generator && (generator = options.generator.trim())) {
+    if ((parts = generator.split(ArrayComprehension.RANGE_SYMBOL)).length > 1) {
+      start = parts[0].split(',').map(toNumber);
+      startIndex = start.length - 1;
+      step = startIndex ? start[startIndex] - start[0] : null;
+
+      output = (startIndex ? [start[0]] : [])
+          .concat(generate(start[startIndex], toNumber(parts[1]), step));
+
+    } else {
+      output = generator.split(',').map(toNumber);
+    }
+  }
+
+  return output;
 }
+
+ArrayComprehension.RANGE_SYMBOL = '..';
--- scripts/ArrayComprehension/ArrayComprehension_test.js
+++ scripts/ArrayComprehension/ArrayComprehension_test.js
@@ -16,20 +16,20 @@ describe('prototype', function() {
     expect(ArrayComprehension({generator: '1   ,  4  '})).toEqual([1, 4]);
   });
   
- /* it("Multiple elements test", function() {
-    assertSimilar(ArrayComprehension({generator: '1,4,6,3,-2'}), [1,4,6,3,-2]);
+  it("Multiple elements test", function() {
+    expect(ArrayComprehension({generator: '1,4,6,3,-2'})).toEqual([1,4,6,3,-2]);
   });
   
   it("Range tests", function() {
-    assertSimilar(ArrayComprehension({generator: '1,3..4'}), [1,3]);
-    assertSimilar(ArrayComprehension({generator: '1,2..2'}), [1,2]);
-    assertSimilar(ArrayComprehension({generator: '3,2..2'}), [3,2]);
-    assertSimilar(ArrayComprehension({generator: '5..3'}), []);
-    assertSimilar(ArrayComprehension({generator: '90..80'}), []);
-    //assertSimilar(ArrayComprehension({generator: '8,4..40'}), [8]);
-    assertSimilar(ArrayComprehension({generator: '1,90..80'}), [1]);
-    assertSimilar(ArrayComprehension({generator: '1..5'}), [1,2,3,4,5]);
-    assertSimilar(ArrayComprehension({generator: '1,4..12'}), [1,4,7,10]);
-    assertSimilar(ArrayComprehension({generator: '12,10..4'}), [12,10,8,6,4]);
-  });*/
+    expect(ArrayComprehension({generator: '1..5'})).toEqual([1,2,3,4,5]);
+    expect(ArrayComprehension({generator: '1,3..4'})).toEqual([1,3]);
+    expect(ArrayComprehension({generator: '1,2..2'})).toEqual([1,2]);
+    expect(ArrayComprehension({generator: '3,2..2'})).toEqual([3,2]);
+    expect(ArrayComprehension({generator: '5..3'})).toEqual([]);
+    expect(ArrayComprehension({generator: '90..80'})).toEqual([]);
+    expect(ArrayComprehension({generator: '8,4..40'})).toEqual([8]);
+    expect(ArrayComprehension({generator: '1,90..80'})).toEqual([1]);
+    expect(ArrayComprehension({generator: '1,4..12'})).toEqual([1,4,7,10]);
+    expect(ArrayComprehension({generator: '12,10..4'})).toEqual([12,10,8,6,4]);
+  });
 });
