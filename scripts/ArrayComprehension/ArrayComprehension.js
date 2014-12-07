5d09f8672d5ab2bcc70b3521e3414abd56b75906
Ivan Straltsou <ivan_straltsou@epam.com>
2014-12-07 23:53:06 +0300

    style fixes

--- scripts/ArrayComprehension/ArrayComprehension.js
+++ scripts/ArrayComprehension/ArrayComprehension.js
@@ -1,37 +1,61 @@
+/**
+ * @function
+ * @name ArrayComprehension
+
+ * @description
+ *
+ */
 function ArrayComprehension(options) {
-  var generator, start, startIndex, step, parts;
-  var output = [];
-
-  function toNumber(item) {
-    return Number(item);
-  }
-
-  function generate(start, end, step) {
-    step = step || 1;
-
-    return (end < start && step > 0) || (end > start && step < 0)?
-           [] :
-           Array.apply(null, new Array(Math.floor((end - start) / step) + 1)).map(function(value, index) {
-             return start + step * index;
-           });
-  }
+  var generator, start, startIndex, parts, output;
 
   // if generator is specified we expect the format to be correct
   if (options && options.generator && (generator = options.generator.trim())) {
-    if ((parts = generator.split(ArrayComprehension.RANGE_SYMBOL)).length > 1) {
-      start = parts[0].split(',').map(toNumber);
-      startIndex = start.length - 1;
-      step = startIndex ? start[startIndex] - start[0] : null;
 
-      output = (startIndex ? [start[0]] : [])
-          .concat(generate(start[startIndex], toNumber(parts[1]), step));
+    parts = generator.split(ArrayComprehension.rangeSymbol);
+
+    if (parts.length > 1) {
+      start = parts[0].split(ArrayComprehension.splitSymbol).map(Number);
+      startIndex = start.length - 1;
+
+      output = (startIndex ? [start[0]] : []).concat(ArrayComprehension.generateRangeItems(
+          start[startIndex],
+          Number(parts[1]),
+          startIndex ? start[startIndex] - start[0] : null
+      ));
 
     } else {
-      output = generator.split(',').map(toNumber);
+      output = generator.split(ArrayComprehension.splitSymbol).map(Number);
     }
+
+  } else {
+    output = [];
   }
 
   return output;
 }
 
-ArrayComprehension.RANGE_SYMBOL = '..';
+ArrayComprehension.rangeSymbol = '..';
+ArrayComprehension.splitSymbol = ',';
+
+/**
+ * @function
+ * @name ArrayComprehension.generateRangeItems
+ *
+ * @param {number} start value of the range
+ * @param {number} end value of the range
+ * @param {number} [step=1] step for generator
+ *
+ * @description
+ * Generates numbers from start value to end value with specified step
+ *
+ * @returns {Array}
+ */
+ArrayComprehension.generateRangeItems = function(start, end, step) {
+  step = step || 1;
+
+  return (end < start && step > 0) || (end > start && step < 0) ?
+         [] :
+         Array.apply(null, new Array(Math.floor((end - start) / step) + 1)).map(function(value, index) {
+           return start + step * index;
+         });
+}
