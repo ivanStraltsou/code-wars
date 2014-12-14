9278d419c27bd6a03d7efab75b1199f63fd53725
Ivan Straltsou <ivan_straltsou@epam.com>
2014-12-14 20:15:21 +0300

    phantom shim added for Function.prototype.bind

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'karma-runtime/es5shim.js',
       'scripts/**/*.js',
       'scripts/**/*_test.js'
     ],
@@ -55,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
