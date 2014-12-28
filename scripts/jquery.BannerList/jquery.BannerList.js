27259e5478c63b3886a67288e62aa0bc059885da
Ivan Straltsou <ivan_straltsou@epam.com>
2014-12-28 15:02:46 +0300

    improvements comments for docco added. fixed implementation added. excluded from tests

--- scripts/jquery.BannerList/jquery.BannerList.js
+++ scripts/jquery.BannerList/jquery.BannerList.js
@@ -1,26 +1,68 @@
+// ### FrontEnd Digest #44 code wars.
+//
+// This doc contains comments for initial [code snippet](https://dl.dropboxusercontent.com/u/69142102/__permanent/jquery.customerName.bannerlist.js)
+//
+//    (c) Ivan Straltsou <ivan_straltsou@epam.com>
+//
 (function($) {
 
+	// Use single object as argument. It's easier to add additional arguments in future and `3` is a lot.
+	//
+	//     /**
+	//      * ...
+	//      * @param {object} options ...
+	//      * @param {string} options.listKey ...
+	//      * @param {string} options.serviceRoot ...
+	//      * @param {function} options.onSuccess ...
+	//      */
+	//      $.fn.BannerList = function(options) {
+	//
+	// Constructor code-style but `new $.fn.BannerList(/*args*/);` will take no effect.
 	$.fn.BannerList = function(listKey, serviceRoot, onSuccess) {
+
+		// Different type of quotes in code.
+		//
+		// There is no need to search for `/`. We just need to check the latest character
+
+		//     if (serviceRoot.slice(-1) !== '/') {
 		if (serviceRoot.lastIndexOf("/") != serviceRoot.length - 1) {
 			serviceRoot = serviceRoot + "/";
 		}
+
+		// Not standard name for this reference. Use `self`, `that`, `this_`.
+		//
+		// Move variables declaration to the very beginning of the function scope.
 		var placeHolder = this;
+
+		// `loadingDiv` id doesn't look like a unique one
 		var list = $('<div id="loadingDiv"></div>');
+
+		// Is fetch logic a responsibility of this plugin?
+		// It should just become data and translate it into markup.
 		$.ajax({
 			url: serviceRoot + "lists/" + listKey,
 			dataType: "json",
 			cache: false,
+
+			// No need to add default values.
 			async: true,
 			success: function(data) {
 				var counter = 0;
 				if (data.ListItems != null) {
+
+					// if data.ListItems is an object property order is not guaranteed across environments
 					$.each(data.ListItems, function(key, listItem) {
 						var cssClass = 'banner';
+
+						// No need in default empty strings.
 						var cssId = '';
 						var cssStyle = '';
 						var imgScr = '';
 						var alt = ' alt=""';
 
+						// It can be a single code-block with current iteration index to construct urls.
+						//
+						// Urls are not customizable through options
 						if (counter == 0) {
 							cssId = 'banner1';
 							cssStyle = 'display: ;';
@@ -36,7 +78,8 @@
 							cssStyle = 'display: none;';
 							imgScr = 'Customers/HiddenName/Content/images/panel/panel3-sml.jpg'
 						}
-						//if (counter == data.ListItems.length - 1) cssClass = 'last';
+
+						// You can use jquery api to add attributes in more intuitive and readable way
 						if (cssClass != '') { cssClass = 'class="' + cssClass + '" '; }
 						if (cssId != '') { cssId = 'id="' + cssId + '" '; }
 						if (cssStyle != '') { cssStyle = 'style="' + cssStyle + '" '; }
@@ -62,11 +105,14 @@
 				}
 				placeHolder.prepend(list);
 
+				// This return statement will take no effect. It should be placed to the end of plugin-function to enable chaining.
 				return this;
 			},
 			error: function(xhr, ajaxOptions, thrownError) {
 				GOD.Ajax.checkError(xhr, ajaxOptions, thrownError);
 			}
 		});
+
+		// Add `return this;` to enable chaining.
 	};
 })(jQuery);
---
+++ scripts/jquery.BannerList/jquery.bannerListNew.js
@@ -0,0 +1,99 @@
+(function($) {
+
+	GOD.Ajax.options = GOD.Ajax.options || {};
+
+	GOD.Ajax.options.bannerList = {
+		root: 'xxx',
+		path: 'lists',
+		key: 'default'
+	};
+
+	GOD.Ajax.getBannerListUrl = function(options) {
+		return [
+			options && options.root || this.options.bannerList.root,
+			options && options.path || this.options.bannerList.path,
+			options && options.url || this.options.bannerList.url
+		].join('/');
+	};
+
+	GOD.Ajax.getBannerList = function(key, root) {
+		return $.ajax({
+			url: GOD.Ajax.getBannerListUrl({
+				root: root,
+				url: key
+			}),
+			dataType: 'json',
+			cache: false,
+			error: function(xhr, ajaxOptions, thrownError) {
+				GOD.Ajax.checkError(xhr, ajaxOptions, thrownError);
+			}
+		});
+	};
+
+	$.fn.bannerList = (function() {
+		var id_ = 0;
+
+		return function(options) {
+			var $list;
+			var self = this;
+
+			options = $.extend($.fn.bannerList.defaults, options);
+
+			$list = $('<div>', {
+				id: options.id + (++id_)
+			});
+
+			GOD.Ajax.getBannerList(options.listKey, options.serviceRoot)
+				.done(function(data) {
+					var index = 0;
+
+					if (data.ListItems) {
+
+						$.each(data.ListItems, function(key, listItem) {
+
+							// use source from properties or generate with index
+							var imageSrc = listItem.Properties ? listItem.Properties[0].Value : options.img.src(index + 1);
+
+							// generate image-link or use name text as content
+							var content = listItem.Link ? options.getImageLink(listItem.Link, imageSrc) : listItem.Name;
+
+							$list.before(
+								$('<div>', {
+											id: [id_, options.itemId, index].join(''),
+											'class': options.itemClassName
+										})
+										.html(content)
+
+										/* show first item only */
+										.toggle(index === 0)
+							);
+
+							index += 1;
+						});
+					}
+
+					options.onSuccess($list);
+					self.prepend($list);
+
+				});
+
+			return this;
+		};
+	});
+
+	$.fn.bannerList.defaults = {
+		id: 'loadingDiv_',
+		itemId: 'banner',
+		itemClassName: 'banner',
+		onSuccess: function() {},
+		getImageLink: function(url, src) {
+			return ['<a href=', url, '> <img src=', src, '/></a>'].join('"');
+		},
+		img: {
+			src: function(id) {
+				return ['Customers/HiddenName/Content/images/panel/panel', id || 1, '-sml.jpg'].join('');
+			}
+		}
+	};
+
+})(jQuery);
